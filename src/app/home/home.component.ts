626a2a4bc7d01fdbee1c78651d148bf4a6c74821
Spikov <hem.jyh@gmail.com>
2022-07-27 10:02:25 +0200

    deleted user template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,16 +11,6 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {    
-    let user: User = new User()
-    user.nom = "toto"
-    user.prenom = "tyty"
-    user.username = "tyto"
-    user.password = "password"
-    user.adresse = "toto's adresse"
-    user.isAdmin = false
-    user.isBanned = false
-
-    sessionStorage.setItem("user", JSON.stringify(user))
   }
 
 }
